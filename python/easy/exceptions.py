4f6be8f73202a49910ef1e843b143ab940cdd070
gianmillare <gianmillaregsm@gmail.com>
2020-06-22 19:12:54 -0700

    easy += exception

---
+++ python/easy/exceptions.py
@@ -0,0 +1,9 @@
+# Exceptions
+# https://www.hackerrank.com/challenges/exceptions/problem
+
+for i in range(int(input())):
+  try:
+    a, b = map(int, input().split())
+    print(a // b)
+  except Exception as e:
+    print("Error Code:", e)
