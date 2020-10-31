23a312e9745e19fe2a7dd29999b069e93ff60e60
gianmillare <gianmillaregsm@gmail.com>
2020-10-31 15:25:25 -0700

    python the minion game, included the two dictionaries and created a string with all vowels

--- python/medium/the_minion_game.py
+++ python/medium/the_minion_game.py
@@ -1,7 +1,12 @@
 # The Minion Game
 # https://www.hackerrank.com/challenges/the-minion-game/problem
- 
+
+# Stuart words MUST start with consonants and Kevins words MUST start with vowels
+stuart, kevin = {}, {}
+
 def minion_game(s):
+    # define the vowels
+    vowels = "aeiou"
     return s
 
 print(minion_game("BANANA"))
