1d19e46e281e90a1c86dafbbe71a16cdfd288439
gianmillare <gianmillaregsm@gmail.com>
2020-10-31 18:16:10 -0700

    py the minion game; including the exact solution posted in HackerRank

--- python/medium/the_minion_game.py
+++ python/medium/the_minion_game.py
@@ -25,4 +25,32 @@ def minion_game(word):
     else:
         return "Draw"
 
+# Solution submitted into HackerRank 
+def minion_game(word):
+    # Define vowels
+    vowels = ["A", "E", "I", "O", "U"]
+
+    # Stuart words MUST start with consonants and Kevins words MUST start with vowels
+    stuart, kevin = 0, 0 
+
+    # split the word into a list with each character
+    word = list(word)
+    
+    for i in range(len(word)):
+        if word[i] in vowels:
+            kevin += len(word) - i
+        else:
+            stuart += len(word) - i
+    
+    if kevin < stuart:
+        print("Stuart %s" % stuart)
+    elif kevin > stuart:
+        print("Kevin %s" % kevin)
+    else:
+        print("Draw")
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
+
 print(minion_game("BANANA"))
