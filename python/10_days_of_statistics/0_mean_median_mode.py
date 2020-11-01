7bdbd33cb395872cbbe573d7badd4ad1fdef26d0
gianmillare <gianmillaregsm@gmail.com>
2020-11-01 09:07:55 -0800

    started 10 days of statistics challenge. pushing start of day 0 mean median and mode

---
+++ python/10_days_of_statistics/0_mean_median_mode.py
@@ -0,0 +1,19 @@
+# Day 0: Mean, Median, and Mode
+# https://www.hackerrank.com/challenges/s10-basic-statistics/problem
+
+# Solution 1: Hard coding the solution
+size = int(input())
+num = input()
+
+def mean(size, num):
+    return num
+
+def median(size, num):
+    return num
+
+def mode(size, num):
+    return num
+
+print(mean(size, num))
+print(median(size, num))
+print(mode(size, num))
