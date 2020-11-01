ef8ecf7eab90c8acb2a914424b06318eba44882a
gianmillare <gianmillaregsm@gmail.com>
2020-10-31 18:15:16 -0700

    completed py medium the minion game

--- python/medium/the_minion_game.py
+++ python/medium/the_minion_game.py
@@ -1,57 +1,28 @@
 # The Minion Game
 # https://www.hackerrank.com/challenges/the-minion-game/problem
 
-# Stuart words MUST start with consonants and Kevins words MUST start with vowels
-stuart, kevin = {}, {}
 # Define vowels
 vowels = ["A", "E", "I", "O", "U"]
 
-# Function to append each letter to its dictionary
-def each_letter(word):
-    for i in word:
-        if i in vowels:
-            if i in kevin:
-                kevin[i] += 1
-            else:
-                kevin[i] = 1
-        else:
-            if i in stuart:
-                stuart[i] += 1
-            else:
-                stuart[i] = 1
-    return None
+def minion_game(word):
 
-# Function to append sub_strings to its dictionary
-def sub_string_left_to_right(word):
-    for i in range(0, len(word)):
+    # Stuart words MUST start with consonants and Kevins words MUST start with vowels
+    stuart, kevin = 0, 0 
+
+    # split the word into a list with each character
+    word = list(word)
+    
+    for i in range(len(word)):
         if word[i] in vowels:
-            sub_string = "".join(word[i:])
-            if sub_string in kevin:
-                kevin[sub_string] += 1
-            else:
-                kevin[sub_string] = 1
+            kevin += len(word) - i
         else:
-            sub_string = "".join(word[i:])
-            if sub_string in stuart:
-                stuart[sub_string] += 1
-            else:
-                stuart[sub_string] = 1
+            stuart += len(word) - i
     
-    return None
-
-def minion_game(s):
-    # Split the string into a list of letters
-    word = list(s.upper())
-
-    # run the first function that will append each letter to its respective dictionary
-    each_letter(word)
-    
-    # run function to append sub_strings left to right
-    sub_string_left_to_right(word)
-    
-    print(stuart)
-    print(kevin)
-
-    return "function complete"
+    if kevin < stuart:
+        return "Stuart %s" % stuart
+    elif kevin > stuart:
+        return "Kevin %s" % kevin
+    else:
+        return "Draw"
 
 print(minion_game("BANANA"))
