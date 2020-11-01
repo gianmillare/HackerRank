437a25ec1c7a9a886fad2b62c45b9013e862bd8e
gianmillare <gianmillaregsm@gmail.com>
2020-10-31 17:58:52 -0700

    py the minion game, completed the function to append substrings to its dictionary

--- python/medium/the_minion_game.py
+++ python/medium/the_minion_game.py
@@ -6,6 +6,7 @@ stuart, kevin = {}, {}
 # Define vowels
 vowels = ["A", "E", "I", "O", "U"]
 
+# Function to append each letter to its dictionary
 def each_letter(word):
     for i in word:
         if i in vowels:
@@ -20,6 +21,23 @@ def each_letter(word):
                 stuart[i] = 1
     return None
 
+# Function to append sub_strings to its dictionary
+def sub_string_left_to_right(word):
+    for i in range(0, len(word)):
+        if word[i] in vowels:
+            sub_string = "".join(word[i:])
+            if sub_string in kevin:
+                kevin[sub_string] += 1
+            else:
+                kevin[sub_string] = 1
+        else:
+            sub_string = "".join(word[i:])
+            if sub_string in stuart:
+                stuart[sub_string] += 1
+            else:
+                stuart[sub_string] = 1
+    
+    return None
 
 def minion_game(s):
     # Split the string into a list of letters
@@ -28,19 +46,8 @@ def minion_game(s):
     # run the first function that will append each letter to its respective dictionary
     each_letter(word)
     
-    # for i in range(0, len(word)):
-    #     if word[i] in vowels:
-    #         sub_string = "".join(word[i:])
-    #         if sub_string in kevin:
-    #             kevin[sub_string] += 1
-    #         else:
-    #             kevin[sub_string] = 1
-    #     else:
-    #         sub_string = "".join(word[i:])
-    #         if sub_string in stuart:
-    #             stuart[sub_string] += 1
-    #         else:
-    #             stuart[sub_string] = 1
+    # run function to append sub_strings left to right
+    sub_string_left_to_right(word)
     
     print(stuart)
     print(kevin)
