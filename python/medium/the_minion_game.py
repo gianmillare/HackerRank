e8b45eb1ba3661cbd2a3165f56cc7b38ce34446d
gianmillare <gianmillaregsm@gmail.com>
2020-10-31 17:18:03 -0700

    py medium the minion game, coded the appendage of the entire inputted variable

--- python/medium/the_minion_game.py
+++ python/medium/the_minion_game.py
@@ -7,6 +7,16 @@ stuart, kevin = {}, {}
 def minion_game(s):
     # define the vowels
     vowels = "aeiou"
+
+    # append the entire word into its respective dictionary
+    if s[0] in vowels:
+        kevin[s] = 1
+    else:
+        stuart[s] = 1
+    
+    
+
+
     return s
 
 print(minion_game("BANANA"))
