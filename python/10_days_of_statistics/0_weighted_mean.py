7923b4aecb979333a3880e072643c31f29a565ef
gianmillare <gianmillaregsm@gmail.com>
2020-11-02 19:19:54 -0800

    started day 0 weighted mean of 10 days of statistics

---
+++ python/10_days_of_statistics/0_weighted_mean.py
@@ -0,0 +1,7 @@
+# Day 0: Weighted Mean
+# https://www.hackerrank.com/challenges/s10-weighted-mean/problem
+
+def weighted_mean(size, x, w):
+    return size
+
+print(weighted_mean("5", "10 40 30 50 20", "1 2 3 4 5"))
