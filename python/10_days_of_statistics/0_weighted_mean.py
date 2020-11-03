95c2ef087a772c45142317651c9b7dbd707e2cae
gianmillare <gianmillaregsm@gmail.com>
2020-11-02 19:41:25 -0800

    completed 10 days of statistics day 0 weighted mean

--- python/10_days_of_statistics/0_weighted_mean.py
+++ python/10_days_of_statistics/0_weighted_mean.py
@@ -8,6 +8,7 @@ def reformat(size, array1, array2):
     w = list(map(int, array2.split(" ")))
     return [size, x, w]
 
+# function to create the multiplied values of each array
 def averaging(size, x, w):
     weighted_averages = []
 
@@ -16,13 +17,46 @@ def averaging(size, x, w):
     
     return weighted_averages
 
+# Main Function
 def weighted_mean(size, x, w):
     reformatted = reformat(size, x, w)
     size, x, w = reformatted[0], reformatted[1], reformatted[2]
 
     weighted_averages = averaging(size, x, w)
-    print(sum(weighted_averages))
-
     
+    return round(sum(weighted_averages) / sum(w), 1)
 
 print(weighted_mean("5", "10 40 30 50 20", "1 2 3 4 5"))
+
+# --------------------Solution submitted into HackerRank ---------------------------
+size = input()
+x = input()
+w = input()
+
+
+# function to reformat inputs into list of integers
+def reformat(size, array1, array2):
+    size = int(size)
+    x = list(map(int, array1.split(" ")))
+    w = list(map(int, array2.split(" ")))
+    return [size, x, w]
+
+# function to create the multiplied values of each array
+def averaging(size, x, w):
+    weighted_averages = []
+
+    for i in range(size):
+        weighted_averages.append(x[i] * w[i])
+    
+    return weighted_averages
+
+# Main Function
+def weighted_mean(size, x, w):
+    reformatted = reformat(size, x, w)
+    size, x, w = reformatted[0], reformatted[1], reformatted[2]
+
+    weighted_averages = averaging(size, x, w)
+    
+    return round(sum(weighted_averages) / sum(w), 1)
+
+print(weighted_mean(size, x, w))
