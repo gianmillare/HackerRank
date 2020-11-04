ed9dbc9aea9722c5dafd423b9a1d1f447aef8814
gianmillare <gianmillaregsm@gmail.com>
2020-11-03 20:01:00 -0800

    started day 1 of statistics quartiles

---
+++ python/10_days_of_statistics/1_quartiles.py
@@ -0,0 +1,22 @@
+# Day 1: Quartiles
+# https://www.hackerrank.com/challenges/s10-quartiles/problem
+
+# Solution 1: Hard coding the answer to understand deeper
+
+# function to return quartile 1
+def q1(array):
+    return array
+
+# function to return quartile 2
+def q2(array):
+    return array
+
+# function to return quartile 3
+def q3(array):
+    return array
+
+# Main Function
+def find_quartiles(size, array):
+    return array
+
+print(find_quartiles("6", "3 7 8 5 12 14 21 13 18")) # 6 | 12 | 16
