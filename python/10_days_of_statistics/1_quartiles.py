38a7379c10a838737f0a32fc334d27492a8654f8
gianmillare <gianmillaregsm@gmail.com>
2020-11-03 21:02:16 -0800

    day 1 stats, quartile; completed quartile 2 of even sized arrays

--- python/10_days_of_statistics/1_quartiles.py
+++ python/10_days_of_statistics/1_quartiles.py
@@ -28,8 +28,11 @@ def q1(array, condition):
             return [lower_bounds, higher_bounds]
 
 # function to return quartile 2; only applicable if size is an even number
-def q2(array, condition):
-    return array
+def q2(array):
+    lower_bounds = array[int(len(array) / 2) - 1]
+    higher_bounds = array[int(len(array) / 2)]
+    return [lower_bounds, higher_bounds]
+    
 
 # function to return quartile 3
 def q3(array, condition):
@@ -55,14 +58,21 @@ def find_quartiles(size, array):
         quartile_3 = int((q3_bounds[0] + q3_bounds[1]) / 2)
     if size % 2 == 0:
         condition = "even"
+        
+        # for quartile 1 of the array
         q1_bounds = q1(array, condition)
         if len(q1_bounds) > 1:
             quartile_1 = int(sum(q1_bounds)) / 2
         else:
             quartile_1 = q1_bounds[0]
+        
+        # for quartile 2 of the array
+        q2_bounds = q2(array)
+        quartile_2 = int(sum(q2_bounds) / 2)
+
 
     
-    return quartile_1
+    return quartile_1, quartile_2
 
 # print(find_quartiles("9", "3 7 8 5 12 14 21 13 18")) # 6 | 12 | 16
 print(find_quartiles("10", "3 7 8 5 12 14 21 13 18 24"))
