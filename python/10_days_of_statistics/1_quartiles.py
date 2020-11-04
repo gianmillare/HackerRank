489322c8a813504dadf4d5c139ceee746bd26175
gianmillare <gianmillaregsm@gmail.com>
2020-11-03 20:55:28 -0800

    day 1 stats, quartile: started working on cases of different array lengths

--- python/10_days_of_statistics/1_quartiles.py
+++ python/10_days_of_statistics/1_quartiles.py
@@ -10,24 +10,35 @@ def reformat(size, array):
     return [size, array]
 
 # function to return quartile 1
-def q1(array):
-    boundary = int(len(array) / 2)
-    sub_array = array[:boundary]
-    lower_bounds = sub_array[int(len(sub_array) / 2) - 1]
-    higher_bounds = sub_array[int(len(sub_array) / 2)]
-    return [lower_bounds, higher_bounds]
+def q1(array, condition):
+    if condition == "odd":
+        boundary = int(len(array) / 2)
+        sub_array = array[:boundary]
+        lower_bounds = sub_array[int(len(sub_array) / 2) - 1]
+        higher_bounds = sub_array[int(len(sub_array) / 2)]
+        return [lower_bounds, higher_bounds]
+    else:
+        boundary = int(len(array) / 2)
+        sub_array = array[:boundary]
+        if len(sub_array) % 2 != 0:
+            return [sub_array[int(len(sub_array) / 2)]]
+        else:
+            lower_bounds = sub_array[int(len(sub_array) / 2) - 1]
+            higher_bounds = sub_array[int(len(sub_array) / 2)]
+            return [lower_bounds, higher_bounds]
 
 # function to return quartile 2; only applicable if size is an even number
-def q2(array):
+def q2(array, condition):
     return array
 
 # function to return quartile 3
-def q3(array):
-    boundary = int(len(array) / 2) + 1
-    sub_array = array[boundary:]
-    lower_bounds = sub_array[int(len(sub_array) / 2) - 1]
-    higher_bounds = sub_array[int(len(sub_array) / 2)]
-    return [lower_bounds, higher_bounds]
+def q3(array, condition):
+    if condition == "odd":
+        boundary = int(len(array) / 2) + 1
+        sub_array = array[boundary:]
+        lower_bounds = sub_array[int(len(sub_array) / 2) - 1]
+        higher_bounds = sub_array[int(len(sub_array) / 2)]
+        return [lower_bounds, higher_bounds]
 
 # Main Function
 def find_quartiles(size, array):
@@ -36,12 +47,23 @@ def find_quartiles(size, array):
     array = sorted(reformatted[1])
 
     if size % 2 != 0:
+        condition = "odd"
         quartile_2 = array[int(size / 2)]
-        q1_bounds = q1(array)
+        q1_bounds = q1(array, condition)
         quartile_1 = int((q1_bounds[0] + q1_bounds[1]) / 2)
-        q3_bounds = q3(array)
+        q3_bounds = q3(array, condition)
         quartile_3 = int((q3_bounds[0] + q3_bounds[1]) / 2)
-    
-    return quartile_1, quartile_2, quartile_3
+    if size % 2 == 0:
+        condition = "even"
+        q1_bounds = q1(array, condition)
+        if len(q1_bounds) > 1:
+            quartile_1 = int(sum(q1_bounds)) / 2
+        else:
+            quartile_1 = q1_bounds[0]
 
-print(find_quartiles("9", "3 7 8 5 12 14 21 13 18")) # 6 | 12 | 16
+    
+    return quartile_1
+
+# print(find_quartiles("9", "3 7 8 5 12 14 21 13 18")) # 6 | 12 | 16
+print(find_quartiles("10", "3 7 8 5 12 14 21 13 18 24"))
+# print(find_quartiles("10", "3 7 8 5 12 14 21 13"))
