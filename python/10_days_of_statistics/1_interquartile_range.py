f2ac79c6d556f07e06656dd5c30a74b8b9fc96d7
gianmillare <gianmillaregsm@gmail.com>
2020-11-06 08:10:30 -0800

    10 days of stats, interquartile range started, pseudocode complete

---
+++ python/10_days_of_statistics/1_interquartile_range.py
@@ -0,0 +1,18 @@
+# Day 1: Interquartile Range
+# https://www.hackerrank.com/challenges/s10-interquartile-range/problem
+
+# Step 1: Reformat the inputs
+
+# Step 2: Create the master list consisting of elements from list x occuring values in list f
+
+# Step 3: Split the master list in half; if odd numbered list, do not include median
+
+# Step 4: Get quartile 1
+
+# Step 5: Get quartile 3
+
+# Main Function:
+def interquartile(size, x, f):
+    return size
+
+print(interquartile("6", "6 12 8 10 20 16", "5 4 3 2 1 5"))
