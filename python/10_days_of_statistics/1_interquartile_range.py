4d71e44372eff6655898c4264091c7eff2865a95
gianmillare <gianmillaregsm@gmail.com>
2020-11-06 08:37:45 -0800

    10 days of stats, interquartile range; completed split_frequencies function

--- python/10_days_of_statistics/1_interquartile_range.py
+++ python/10_days_of_statistics/1_interquartile_range.py
@@ -17,10 +17,17 @@ def value_times_frequency(size, x_array, f_array):
             element_by_frequency.append(x_array[i])
     
     return element_by_frequency
-    
-        
 
 # Step 3: Split the master list in half; if odd numbered list, do not include median
+def split_frequency(x):
+    if len(x) % 2 != 0:
+        lower_half = x[:int(len(x) / 2)]
+        upper_half = x[int(len(x) / 2) + 1:]
+    else:
+        lower_half = x[:int(len(x) / 2)]
+        upper_half = x[int(len(x) / 2):]
+    
+    return [lower_half, upper_half]
 
 # Step 4: Get quartile 1
 
@@ -33,9 +40,14 @@ def interquartile(size, x, f):
     reformatted = reformat(size, x, f)
     size, x_array, f_array = reformatted[0], reformatted[1], reformatted[2]
 
-    # create master list of element * frequency
-    element_by_frequency = value_times_frequency(size, x_array, f_array)
+    # create master list of element * frequency, sort by increasing value
+    element_by_frequency = sorted(value_times_frequency(size, x_array, f_array))
+
+    # split the frequency list in half 
+    split_frequencies = split_frequency(element_by_frequency)
+    lower_half = split_frequencies[0]
+    upper_half = split_frequencies[1]
     
-    return element_by_frequency
+    return upper_half
 
 print(interquartile("6", "6 12 8 10 20 16", "5 4 3 2 1 5"))
