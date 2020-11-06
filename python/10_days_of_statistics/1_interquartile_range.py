f6605d975ebf53c34841c43573b1331aa340bcf2
gianmillare <gianmillaregsm@gmail.com>
2020-11-06 09:13:56 -0800

    10 days of stats, interquartile range; debugged code that was preventing solution from printing float

--- python/10_days_of_statistics/1_interquartile_range.py
+++ python/10_days_of_statistics/1_interquartile_range.py
@@ -71,4 +71,7 @@ def interquartile(size, x, f):
     return third_quartile - first_quartile
 
 # print(interquartile("6", "6 12 8 10 20 16", "5 4 3 2 1 5"))
-print(interquartile("6", "6 12 8 10 20 16", "5 4 3 2 1 4"))
+# print(interquartile("6", "6 12 8 10 20 16", "5 4 3 2 1 4"))
+print(interquartile("30", 
+                    "10 40 30 50 20 10 40 30 50 20 1 2 3 4 5 6 7 8 9 10 20 10 40 30 50 20 10 40 30 50", 
+                    "1 2 3 4 5 6 7 8 9 10 1 2 3 4 5 6 7 8 9 10 10 40 30 50 20 10 40 30 50 20"))
