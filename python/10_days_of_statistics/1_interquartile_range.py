4ac73119a378b51934c3634481dd677bc72fce3d
gianmillare <gianmillaregsm@gmail.com>
2020-11-06 08:27:21 -0800

    10 days of stats, interquartile range; completed value * frequency function to return master list

--- python/10_days_of_statistics/1_interquartile_range.py
+++ python/10_days_of_statistics/1_interquartile_range.py
@@ -9,6 +9,16 @@ def reformat(size, x, f):
     return [size, x_array, f_array]
 
 # Step 2: Create the master list consisting of elements from list x occuring values in list f
+def value_times_frequency(size, x_array, f_array):
+    element_by_frequency = []
+    
+    for i in range(size):
+        for j in range(f_array[i]):
+            element_by_frequency.append(x_array[i])
+    
+    return element_by_frequency
+    
+        
 
 # Step 3: Split the master list in half; if odd numbered list, do not include median
 
@@ -18,9 +28,14 @@ def reformat(size, x, f):
 
 # Main Function:
 def interquartile(size, x, f):
+
+    # reformat the inputs
     reformatted = reformat(size, x, f)
     size, x_array, f_array = reformatted[0], reformatted[1], reformatted[2]
+
+    # create master list of element * frequency
+    element_by_frequency = value_times_frequency(size, x_array, f_array)
     
-    return size, x_array, f_array
+    return element_by_frequency
 
 print(interquartile("6", "6 12 8 10 20 16", "5 4 3 2 1 5"))
