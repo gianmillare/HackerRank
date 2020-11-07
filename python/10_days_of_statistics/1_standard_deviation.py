6bedbf274655876af84951a720405e17072a6dee
gianmillare <gianmillaregsm@gmail.com>
2020-11-07 10:53:28 -0800

    10 days of stats, standard deviation; started the challenge, completed the pseudocode

---
+++ python/10_days_of_statistics/1_standard_deviation.py
@@ -0,0 +1,26 @@
+# Day 1: Standard Deviation
+# https://www.hackerrank.com/challenges/s10-standard-deviation/problem
+
+# Standard Deviation formula: sqrt(sum((value - mean)^2) / N)
+
+# Step 1: reformat the input into an integer-readable format
+def reformat(size, array):
+    return array
+
+# Step 2: find the mean of the array
+def array_mean(size, array):
+    return array
+
+# Step 3: create a new array consisting of all results from (value - mean)^2 and return its sum
+def squared_distance(size, array):
+    return array
+
+# Step 4: Complete the function by dividing the summed squared distance by the number of values, and square root the result
+def square_root_distances(size, array):
+    return array
+
+# Main Function
+def standard_deviation(size, array):
+    return array
+
+print(standard_deviation("5", "10 40 30 50 20"))
