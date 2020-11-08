de6961bcba5b1dc2673ad97c9f635f1002c1309a
gianmillare <gianmillaregsm@gmail.com>
2020-11-08 10:46:45 -0800

    10 days of stats, basic probability; created dictionary depicting the probability of both die

---
+++ python/10_days_of_statistics/2_basic_probability.py
@@ -0,0 +1,15 @@
+# Day 2: Basic Probability
+# https://www.hackerrank.com/challenges/s10-mcq-1/problem
+
+# Question 1: In a single toss of 2 fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most 9.
+# Die 1: [1, 2, 3, 4, 5, 6], Die 2: [1, 2, 3, 4, 5, 6]
+
+# Possibilities 9 or below:
+possibilities = {
+    1: [1, 2, 3, 4, 5, 6], # 6 outcomes, max 7
+    2: [1, 2, 3, 4, 5, 6], # 6 outcomes, max 8
+    3: [1, 2, 3, 4, 5, 6], # 6 outcomes, max 9
+    4: [1, 2, 3, 4, 5], # 5 outcomes, max 9
+    5: [1, 2, 3, 4], # 4 outcomes, max 9
+    6: [1, 2, 3] # 3 outcomes, max 9
+}
