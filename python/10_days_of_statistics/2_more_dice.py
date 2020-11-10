6c995dd9afa876edf7979c7c18acbe3da67575aa
Gian Millare <gianmillaregsm@gmail.com>
2020-11-09 19:27:00 -0800

    10 days of stats, more dice; completed the challenge. no code required

--- python/10_days_of_statistics/2_more_dice.py
+++ python/10_days_of_statistics/2_more_dice.py
@@ -5,9 +5,19 @@
 # find the probability that the values rolled by each die will be different and the two dice have a sum of 6.
 
 # Create a dictionary of all the possible sets of dice
-possibilities = [
+possibilities = {
     1: [2, 4],
     2: [4, 2],
     3: [5, 1],
     4: [1, 5]
-]
+}
+
+# We know that the total possible rolls for the dice is 6 x 6 == 36 so...
+total_possible = 36
+
+def probability(possibilities):
+    return str(len(possibilities)) + " / " + str(total_possible)
+print(probability(possibilities))
+
+# ------------------- ANSWER SUBMITTED INTO HACKERRANK ----------------------
+# 1 / 9
