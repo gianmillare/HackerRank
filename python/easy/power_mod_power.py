4095f2b77f7335845235bbfdc97c5c0c2e9c36a5
gianmillare <gianmillaregsm@gmail.com>
2020-11-21 08:41:21 -0800

    python easy power mod power; reintroduction to solving challenges

---
+++ python/easy/power_mod_power.py
@@ -0,0 +1,15 @@
+# Power - Mod Power
+# https://www.hackerrank.com/challenges/python-power-mod-power/problem
+
+# Take the inputs as they are
+a = input()
+b = input()
+m = input()
+
+# Reformat the inputs so they are readable by the mod_power function
+def reformat(a, b, m):
+    return a, b, m
+
+# Main
+def mod_power(a, b, m):
+    return a, b, m
