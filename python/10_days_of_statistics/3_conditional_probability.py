8a3d7a900df15e54fccda68500ec9b5194486fa4
gianmillare <gianmillaregsm@gmail.com>
2020-11-23 20:37:01 -0800

    10 days of stats, Conditional Probability. Started.

---
+++ python/10_days_of_statistics/3_conditional_probability.py
@@ -0,0 +1,10 @@
+# Day 3: Conditional Probability
+# https://www.hackerrank.com/challenges/s10-mcq-4/problem
+
+# Objective:In this challenge, we get started with conditional probability
+
+# Suppose a family has 2 children, one of which is a boy. What is the probability that both children are boys?
+def probability():
+    return
+
+# Contingencies: at least one child is a boy
