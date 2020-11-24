958499dbeb24db8b3ce6eaea6121d78982b22a72
gianmillare <gianmillaregsm@gmail.com>
2020-11-23 20:44:17 -0800

    10 days of stats, conditional probability. Completed

--- python/10_days_of_statistics/3_conditional_probability.py
+++ python/10_days_of_statistics/3_conditional_probability.py
@@ -4,7 +4,11 @@
 # Objective:In this challenge, we get started with conditional probability
 
 # Suppose a family has 2 children, one of which is a boy. What is the probability that both children are boys?
-def probability():
-    return
 
 # Contingencies: at least one child is a boy
+
+# First list out the possible outcomes of children for a family with two children
+possible_matches = ["BB", "BG", "GB", "GG"]
+answer = 1 / len(possible_matches) # However, only three of these staisfy the contingency mentioned above
+
+result = 1 / 3
