857cf4a37674ae73150080dee16a1fbdcbdf89f1
gianmillare <gianmillaregsm@gmail.com>
2020-11-23 20:48:25 -0800

    10 days of statistics, conditional probability. Adding another interpretation

--- python/10_days_of_statistics/3_conditional_probability.py
+++ python/10_days_of_statistics/3_conditional_probability.py
@@ -12,3 +12,14 @@ possible_matches = ["BB", "BG", "GB", "GG"]
 answer = 1 / len(possible_matches) # However, only three of these staisfy the contingency mentioned above
 
 result = 1 / 3
+
+# ------------------------------------ Other Interpretations ---------------------------------------
+
+# Some people interpret the question as a trick, believing that the Probability of the second child gender is independent of the first
+
+first_child = "Boy"
+probability_of_second_child_gender = 1 / 2
+
+# Assume that 1 = True and 0 = False for gender equals boy
+# P(B | A) --> 1 * 1/2 = 1/2
+
