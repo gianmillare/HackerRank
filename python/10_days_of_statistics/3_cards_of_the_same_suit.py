dda7556d8dc1e26c68833af4f2e6b2a52c90f163
gianmillare <gianmillaregsm@gmail.com>
2020-11-23 20:56:37 -0800

    10 days of stats, cards of the same suit. Started.

---
+++ python/10_days_of_statistics/3_cards_of_the_same_suit.py
@@ -0,0 +1,9 @@
+# Day 3: Cards of the Same Suit
+# https://www.hackerrank.com/challenges/s10-mcq-5/problem
+
+# You draw 2 cards from a standard 52-card deck without replacing them. What is the probability that both cards are of the same suit
+
+number_of_cards = 52
+
+def pick_one_card(number_of_cards):
+    return number_of_cards - 1
