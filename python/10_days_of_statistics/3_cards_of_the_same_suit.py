af896dcdd7b4d40daf767aa1ca41cb6f995cdf2d
gianmillare <gianmillaregsm@gmail.com>
2020-11-23 21:03:29 -0800

    10 days of stats, cards of the same suit. completed

--- python/10_days_of_statistics/3_cards_of_the_same_suit.py
+++ python/10_days_of_statistics/3_cards_of_the_same_suit.py
@@ -7,3 +7,23 @@ number_of_cards = 52
 
 def pick_one_card(number_of_cards):
     return number_of_cards - 1
+
+# Once we select the first card, the total number changes to 51
+total_cards = pick_one_card(number_of_cards)
+print(total_cards)
+
+# We also know that there are 13 suits in a deck with 4 pairs of each number --> 52
+number_of_suits = 13
+
+# Once we select the first card, the total number of suits needed to match decreases by 1
+def pick_one_suit(number_of_suits):
+    return number_of_suits - 1
+
+total_suits = pick_one_suit(number_of_suits)
+print(total_suits)
+
+# The probability then changes to the number of suits over the total number of cards
+def probability(total_cards, total_suits):
+    return str(total_suits) + " / " + str(total_cards)
+
+print(probability(total_cards, total_suits))
