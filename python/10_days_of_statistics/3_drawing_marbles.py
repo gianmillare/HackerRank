b77b8251a6b88a94f862aba98f57aa684384f00f
gianmillare <gianmillaregsm@gmail.com>
2020-11-24 19:25:32 -0800

    10 days of stats, drawing marbles; completed challenge

--- python/10_days_of_statistics/3_drawing_marbles.py
+++ python/10_days_of_statistics/3_drawing_marbles.py
@@ -14,3 +14,15 @@ number_blue = 4
 # Method: marbles taken out one by one, first is red
 new_red = number_red - 1
 new_total = new_red + number_blue
+
+# Chances of next marble drawn being blue
+def probability(x, y):
+    return x / y
+
+print(probability(number_blue, new_total)) # 0.6666666666666666
+
+# Options
+print(1 / 12) # 0.08333333333333333
+print(1 / 6) # 0.16666666666666666
+print(7 / 12) # 0.5833333333333334
+print(2 / 3) # 0.6666666666666666 answer
