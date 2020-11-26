81ab461f97e468d8342801e8df87edfbbda2db99
Gian Millare <gianmillaregsm@gmail.com>
2020-11-26 11:40:21 -0800

    restructuring 10 days of stats, binomial distribution challenge

--- python/10_days_of_statistics/4_binomial_distribution_I.py
+++ python/10_days_of_statistics/4_binomial_distribution_I.py
@@ -3,19 +3,10 @@
 
 # Task: The ratio of boys to girls for babies born in Russia is 1.09:1. If there is 1 child born per birth, what proportion of Russian families with exactly 6 children will have at least 3 boys?
 
-# What do we know
-ratio_1 = input()
-ratio_2 = input()
+# Assign both inputs to variables
+ratio_1, ratio_2 = list(map(float, input().split(" ")))
+ratio = ratio_1 / ratio_2
 
-desired_outcome = 3 / 6
-
-# Reformat the input
-def reformat(ratio_1, ratio_2):
-    return list(map(int, [ratio_1, ratio_2]))
-
-# Main Function
-def binomial(ratio_1, ratio_2, desired_outcome):
-    reformatted_ratio = reformat(ratio_1, ratio_2)
-    return reformatted_ratio
-
-print(binomial(ratio_1, ratio_2, desired_outcome))
+# Function to run factorial formula
+def fact(n):
+    return 1 if n == 0 else n*fact(n-1)
