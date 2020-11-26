c31ed91853d8de5f0415289392baf22b7ffe802e
gianmillare <gianmillaregsm@gmail.com>
2020-11-25 18:38:44 -0800

    10 days of stats, binomial distribution. Adding functions for main and to reformat an input

--- python/10_days_of_statistics/4_binomial_distribution_I.py
+++ python/10_days_of_statistics/4_binomial_distribution_I.py
@@ -3,6 +3,14 @@
 
 # Task: The ratio of boys to girls for babies born in Russia is 1.09:1. If there is 1 child born per birth, what proportion of Russian families with exactly 6 children will have at least 3 boys?
 
-def binomial(ratio):
-    return ratio
+# What do we know
+boys_to_girls = 1.09 / 1
+desired_outcome = 3 / 6
 
+# Reformat the input
+def reformat(inputs):
+    return inputs.split()
+
+# Main Function
+def binomial(ratio, probability):
+    return
