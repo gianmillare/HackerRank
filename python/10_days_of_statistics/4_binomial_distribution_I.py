0adf549880f768ca8bb7ffc59ecfc04e757e00e8
gianmillare <gianmillaregsm@gmail.com>
2020-11-26 12:24:05 -0800

    10 days of stats, binomial distribution. debugged issue that was causing a recursion error

--- python/10_days_of_statistics/4_binomial_distribution_I.py
+++ python/10_days_of_statistics/4_binomial_distribution_I.py
@@ -3,11 +3,6 @@
 
 # Task: The ratio of boys to girls for babies born in Russia is 1.09:1. If there is 1 child born per birth, what proportion of Russian families with exactly 6 children will have at least 3 boys?
 
-# Assign both inputs to variables
-ratio_1, ratio_2 = list(map(float, input().split(" ")))
-odds = ratio_1 / ratio_2
-probability = odds / (odds + 1)
-
 # Function to run factorial formula
 def fact(total):
     return 1 if total == 0 else total * fact(total - 1)
@@ -17,7 +12,12 @@ def permutate(total, successes):
     return fact(total) / (fact(successes) * fact(total - successes))
 
 # Main: Binomial Distribution formula written in code
-def binomial_dist(total, successes, probability):
+def binomial_dist(successes, total, probability):
     return permutate(total, successes) * probability**successes * (1 - probability)**(total-successes)
+
+# Assign both inputs to variables
+ratio_1, ratio_2 = list(map(float, input().split(" ")))
+odds = ratio_1 / ratio_2
+probability = odds / (odds + 1)
 
 print(round(sum([binomial_dist(i, 6, probability) for i in range(3, 7)]), 3))
