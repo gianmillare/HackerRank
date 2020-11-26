69e709144b917448ba4050aa1af2940c44538501
gianmillare <gianmillaregsm@gmail.com>
2020-11-25 18:27:00 -0800

    10 days of stats, binomial distribution I. started the challenge.

---
+++ python/10_days_of_statistics/4_binomial_distribution_I.py
@@ -0,0 +1,8 @@
+# Day 4: Binomial Distribution I
+# https://www.hackerrank.com/challenges/s10-binomial-distribution-1/problem
+
+# Task: The ratio of boys to girls for babies born in Russia is 1.09:1. If there is 1 child born per birth, what proportion of Russian families with exactly 6 children will have at least 3 boys?
+
+def binomial(ratio):
+    return ratio
+
